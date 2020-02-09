0e1911b369fa86da693acdb139d2b423ceccaa51
Owen Abbott <ocabbott@gmail.com>
2020-02-09 23:57:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -64,7 +64,14 @@ def apply_coupons(cart, coupons)
   cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
   if cart_item && cart_item[:count] >= coupons[counter][:num]
     if cart_item_with_coupon 
-      cart_item_with_coupon
+      cart_item_with_coupon[:count] += coupons[counter][:num]
+      cart_item[:count] -= coupons[counter][:num]
+    else
+      cart_item_with_coupon = {:item => couponed_item_name,
+      :price => coupons[counter][:cost] / coupons[counter][:num], 
+        :count => coupons[counter][:num], 
+        :clearance => cart_item[:clearance]
+      }
       end
     end
   counter += 1 

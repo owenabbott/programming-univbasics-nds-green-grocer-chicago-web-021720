e171ddf1d9d83aed289e116784905b0236e8b9bb
Owen Abbott <ocabbott@gmail.com>
2020-02-09 23:46:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,8 @@ def apply_coupons(cart, coupons)
   while counter < coupons.length 
   
   cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-  couponed_item_name = "#{coupons[counter][:item]}"
+  couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = 
   counter += 1 
   
   

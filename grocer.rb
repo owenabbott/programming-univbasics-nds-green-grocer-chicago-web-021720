c666204634a0a6cacb60eccb28d12d3bd0a642af
Owen Abbott <ocabbott@gmail.com>
2020-02-10 00:20:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -98,8 +98,8 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart=consolidate_cart(cart)
-  couponed_cart = apply_coupons
-  
+  couponed_cart = apply_coupons(consolidated_cart)
+  clearanced_cart = apply_clearance(couponed_cart)
 #  pp coupons
   # Consult README for inputs and outputs
   #
